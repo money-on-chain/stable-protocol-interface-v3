1e9868fb0a4ce03a49d1c9cc09cade83c2ffd2bd
mulonemartin <mulone.martin@gmail.com>
2025-07-01 14:45:49 -0300

    fix: convert send to typescript

--- src/components/Send/index.tsx
+++ src/components/Send/index.tsx
@@ -17,33 +17,33 @@ import InputAmount from "../InputAmount";
 import { fromContractPrecisionDecimals } from "../../helpers/Formats";
 import ModalConfirmSend from "../Modals/ConfirmSend";
 
-export default function Send() {
+export default function Send(): JSX.Element {
     const { t, i18n } = useProjectTranslation();
     const auth = useContext(AuthenticateContext);
 
-    const tokenSend = tokenExchange();
+    const tokenSend: string[] = tokenExchange();
     // Add Token Govern
     tokenSend.push("TG");
     // Add Coinbase support at index 0
     tokenSend.splice(0, 0, "COINBASE");
 
-    const defaultTokenSend = tokenSend[0];
-    const [currencyYouSend, setCurrencyYouSend] = useState(defaultTokenSend);
+    const defaultTokenSend: string = tokenSend[0];
+    const [currencyYouSend, setCurrencyYouSend] = useState<string>(defaultTokenSend);
 
-    const [amountYouSend, setAmountYouSend] = useState("");
-    const [destinationAddress, setDestinationAddress] = useState("");
+    const [amountYouSend, setAmountYouSend] = useState<string>("");
+    const [destinationAddress, setDestinationAddress] = useState<string>("");
 
-    const [sendingUSD, setSendingUSD] = useState(new BigNumber(0));
+    const [sendingUSD, setSendingUSD] = useState<BigNumber>(new BigNumber(0));
 
     //const [isDirtyYouSend, setIsDirtyYouSend] = useState(false);
 
     const [inputValidationErrorText, setInputValidationErrorText] =
-        useState("");
+        useState<string>("");
     const [
         inputValidationAddressErrorText,
         setInputValidationAddressErrorText,
-    ] = useState("");
-    const [inputValidationError, setInputValidationError] = useState(false);
+    ] = useState<string>("");
+    const [inputValidationError, setInputValidationError] = useState<boolean>(false);
 
     useEffect(() => {
         setAmountYouSend(amountYouSend);
@@ -61,29 +61,29 @@ export default function Send() {
         }
     }, [destinationAddress]);
 
-    const onChangeCurrencyYouSend = (newCurrencyYouExchange) => {
+    const onChangeCurrencyYouSend = (newCurrencyYouExchange: string): void => {
         onClear();
         setCurrencyYouSend(newCurrencyYouExchange);
     };
 
-    const onClear = () => {
+    const onClear = (): void => {
         //setIsDirtyYouSend(false);
         setAmountYouSend("");
     };
 
-    const onValidate = () => {
-        let amountInputError = false;
-        let addressInputError = false;
+    const onValidate = (): void => {
+        let amountInputError: boolean = false;
+        let addressInputError: boolean = false;
 
         // 1. User Send Token Validation
-        const totalBalance = new BigNumber(
+        const totalBalance: BigNumber = new BigNumber(
             fromContractPrecisionDecimals(
                 TokenBalance(auth, currencyYouSend),
                 TokenSettings(currencyYouSend).decimals
             )
         );
         console.log("amount you send", amountYouSend);
-        const amountYouSendBig = new BigNumber(amountYouSend);
+        const amountYouSendBig: BigNumber = new BigNumber(amountYouSend);
         if (amountYouSendBig.gt(totalBalance)) {
             setInputValidationErrorText(t("send.infoNoBalance"));
             amountInputError = true;
@@ -125,28 +125,28 @@ export default function Send() {
         }
     };
 
-    const onChangeAmountYouSend = (newAmount, isPriceOnly = false) => {
-        const newAmountBig = new BigNumber(newAmount);
+    const onChangeAmountYouSend = (newAmount: string | number, isPriceOnly: boolean = false): void => {
+        const newAmountBig: BigNumber = new BigNumber(newAmount);
         if (!isPriceOnly) {
             //setIsDirtyYouSend(true);
-            setAmountYouSend(newAmount);
+            setAmountYouSend(newAmount.toString());
         }
 
-        let caIndex = 0
-        const aCurrencyYouSend = currencyYouSend.split("_");
+        let caIndex: number = 0;
+        const aCurrencyYouSend: string[] = currencyYouSend.split("_");
         switch (aCurrencyYouSend[0]) {
             case "CA":
-                caIndex = parseInt(aCurrencyYouSend[1])
+                caIndex = parseInt(aCurrencyYouSend[1]);
                 break;
             case "TC":
-                caIndex = parseInt(aCurrencyYouSend[1])
+                caIndex = parseInt(aCurrencyYouSend[1]);
                 break;
             case "TP":
-                caIndex = 0
+                caIndex = 0;
                 break;
         }
 
-        const convertAmount = ConvertAmount(
+        const convertAmount: BigNumber = ConvertAmount(
             auth,
             currencyYouSend,
             `CA_${caIndex}`,
@@ -154,14 +154,14 @@ export default function Send() {
             false
         );
 
-        const priceCA = new BigNumber(
+        const priceCA: BigNumber = new BigNumber(
             fromContractPrecisionDecimals(
                 auth.contractStatusData[caIndex].PP_CA[0],
                 settings.tokens.CA[caIndex].decimals
             )
         );
 
-        let convertAmountUSD;
+        let convertAmountUSD: BigNumber;
         if (currencyYouSend === "COINBASE") {
             convertAmountUSD = convertAmount;
         } else {
@@ -171,7 +171,7 @@ export default function Send() {
         setSendingUSD(convertAmountUSD);
     };
 
-    const onChangeDestinationAddress = (event) => {
+    const onChangeDestinationAddress = (event: React.ChangeEvent<HTMLInputElement>): void => {
         if (event.target.value.length < 42) {
             setInputValidationAddressErrorText(t("send.infoAddressInvalid"));
             setInputValidationError(true);
@@ -179,11 +179,11 @@ export default function Send() {
         setDestinationAddress(event.target.value);
     };
 
-    const setAddTotalAvailable = () => {
+    const setAddTotalAvailable = (): void => {
         //setIsDirtyYouSend(false);
 
-        const tokenSettings = TokenSettings(currencyYouSend);
-        const totalYouSend = new BigNumber(
+        const tokenSettings: any = TokenSettings(currencyYouSend);
+        const totalYouSend: BigNumber = new BigNumber(
             fromContractPrecisionDecimals(
                 TokenBalance(auth, currencyYouSend),
                 tokenSettings.decimals
