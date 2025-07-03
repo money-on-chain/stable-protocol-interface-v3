f4b198dd979fca6b0e07855a24e7a0a55aedecd4
mulonemartin <mulone.martin@gmail.com>
2025-07-03 09:29:29 -0300

    fix: unused piece of code

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,32 +5,10 @@ import HomeTabs from "../../components/PortfolioOperationsTabs";
 
 import "./Styles.scss";
 
-interface TabItem {
-    name: string;
-    content: React.ReactElement;
-}
 
 export default function Home(): React.ReactElement {
     const isMobile = window.matchMedia("(max-width: 767px)").matches;
-
-    // Tabs for mobile
-    const Tab1 = (): React.ReactElement => (
-        <div className="dashboard-portfolio">
-            <Portfolio />
-        </div>
-    );
-
-    const Tab2 = (): React.ReactElement => (
-        <div className="content-last-operations">
-            <LastOperations token={"all"}></LastOperations>
-        </div>
-    );
-
-    const tabs: TabItem[] = [
-        { name: "Portfolio", content: <Tab1 /> },
-        { name: "Last Operations", content: <Tab2 /> },
-    ];
-
+    
     return (
         <>
             {isMobile ? (
