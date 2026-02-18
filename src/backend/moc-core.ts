1fa9c1c3bb591003ac3bf3882bbeb44fc8afc8c2
Martin Mulone <mulone.martin@gmail.com>
2026-02-18 15:23:02 -0300

    fix: remove debug

--- src/backend/moc-core.ts
+++ src/backend/moc-core.ts
@@ -635,17 +635,7 @@ const redeemTCandTP = async (
         throw new Error(
             `Insufficient ${(settings.tokens.CA[caIndex] as TokenConfig).name} in the contract. Balance: ${caBalance} ${(settings.tokens.CA[caIndex] as TokenConfig).name}`
         );
-
-    //qTP = toBigIntPrecision(2000, 18);    
-    //limitAmount = toBigIntPrecision(0.0001, 18);    
     
-    console.log("DEBUG>>>")        
-    console.log("tpAddress", tpAddress);
-    console.log("qTC", fromWei(qTC));
-    console.log("qTP", fromWei(qTP));
-    console.log("limitAmount", fromWei(limitAmount));    
-    console.log("DEBUG<<<");
-
     return await sendWithExecFee(
         context,
         onTransaction,
